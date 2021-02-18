e0893f42c570cee6778b2ea13265dc23fa539fea
Vladimir101 <vladimirbelor100@gmail.com>
2021-02-17 21:43:04 -0800

    version 2

---
+++ SeleniumForJenkins/src/test/java/selenium/Selenium2Test.java
@@ -0,0 +1,44 @@
+package selenium;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class Selenium2Test
+{
+	private WebDriver driver;
+
+	@BeforeAll
+	static void oneTimeSetUp()
+	{
+		WebDriverManager.chromedriver().setup();
+	}
+	
+	@BeforeEach
+	void setUp()
+	{
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	void openSchoolWebsite()
+	{
+		System.out.println("Hi, Russian QA in Silicon Valley!");
+		driver.get("https://www.facebook.com/groups/365258477409510");
+		String title = driver.getTitle();
+		assertEquals("Russian QA in Silicon Valley", title);
+	}
+	
+	@AfterEach
+	void tearDown()
+	{
+		driver.quit();
+	}
+}
